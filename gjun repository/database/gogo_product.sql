13af4c3c84208e73e98a7c6b0253d78b284030c8
Anne Shi <wjanne1019@gmail.com>
2023-08-23 18:29:19 +0800

    Product0823

--- gjun repository/database/gogo_product.sql
+++ gjun repository/database/gogo_product.sql
@@ -45,7 +45,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES ('456','','','',NULL,NULL,'',NULL,'',''),('5','','','',NULL,NULL,'',NULL,'',''),('5,5','','','',1,NULL,'',NULL,'',''),('gogop23000P','','','',NULL,NULL,'',NULL,'',''),('gogop23000Q','','','',NULL,NULL,'',NULL,'',''),('gogop23000T','1','1','1',1,1,'1',1,'1','1'),('gogop2308210001','purse','Aglea','手提包無背帶',1,5280,'black',30,'x','./images/imgf01.jpg'),('gogop2308210002','shoulder bag','Thalia','多層次夾層收納',1,3980,'brown',25,'x','./images/imgf22.jpg'),('gogop2308210003','purse','Canvas bag','編織可調整背帶',1,3680,'skin color',35,'x','./images/imgf36.jpg');
+INSERT INTO `product` VALUES ('456','','','',NULL,NULL,'',NULL,'',''),('5','','','',NULL,NULL,'',NULL,'',''),('5,5','','','',1,NULL,'',NULL,'',''),('gogop23000P','','','',NULL,NULL,'',NULL,'',''),('gogop23000Q','','','',NULL,NULL,'',NULL,'',''),('gogop23000T','1','1','1',1,1,'1',1,'1','1'),('gogop2308210001','purse','Aglea','手提包無背帶',1,5280,'black',30,'x','./images/imgf01.jpg'),('gogop2308210002','shoulder bag','Thalia','多層次夾層收納',1,3980,'brown',25,'x','./images/imgf22.jpg'),('gogop2308210003','purse','Canvas bag','編織可調整背帶',1,3680,'skin color',35,'x','./images/imgf36.jpg'),('gogop2308210004,456','bag','Urbano','直式托特包',1,40889,'dark brown',20,'x','./images/img143.jpg'),('gogop2308210004,gogop2308210004,456','bag','Urbano','直式托特包',1,40889,'dark brown',20,'x','./images/img143.jpg'),('gogop2308210005','backpack','Vainno','商務筆電後背包',1,28431,'black',38,'x','./images/img319.jpg');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-23 17:49:27
+-- Dump completed on 2023-08-23 18:27:38
