6b397afc586cc2e39318ba76429d89c41febbe31
Anne Shi <wjanne1019@gmail.com>
2023-08-26 12:39:02 +0800

    product0826 1238

---
+++ gjun repository/gogo product/sbgogo0822-3/src/main/java/com/example/demo/Sbgogo08223Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbgogo08223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbgogo08223Application.class, args);
+	}
+
+}
